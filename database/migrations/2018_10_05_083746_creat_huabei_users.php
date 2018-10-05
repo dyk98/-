96659baed2ca2f9c474ce79d486d96fbd72205c6
董原恺 <124449645@qq.com>
2018-10-05 17:23:19 +0800

    '建表'

--- database/migrations/2018_10_05_083746_creat_huabei_users.php
+++ database/migrations/2018_10_05_083746_creat_huabei_users.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreatHuabeiUsers extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,12 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        //huabei_users
+        Schema::create('users' , function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',255);
+            $table->string('username',255);
+            $table->string('password',255);
         });
     }
 
@@ -27,6 +29,7 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::drop('users');
+        //
     }
 }
