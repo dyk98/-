8040b8e873b035cc2b3cccc77e5cbcdda5e5d82c
董原恺 <124449645@qq.com>
2018-10-05 21:26:17 +0800

    'Cookie'

--- app/Http/Controllers/UserCookie.php
+++ app/Http/Controllers/UserCookie.php
@@ -11,8 +11,8 @@ class UserCookie extends Controller
     }
     //
     public function RegisterCookie (Request $request) {
-        $value = response('Hello Cookie')->cookie('mzl','mzlsb','3600');
-        $values = $request->cookie('mzl');
-        $this->print_encode($values);
+        return response('Hello Lol')->cookie(
+            'mzl', 'mzlsb', 3600
+        );
     }
 }
